5548b0dd7f8648504caf30573b08af320345565e
nail14 <nail_14@mail.ru>
2022-10-24 10:39:00 +0300

    lesson-1.2

--- users/models.py
+++ users/models.py
@@ -9,17 +9,6 @@ from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
-
-
-# class Author(models.Model):
-#     first_name = models.CharField(max_length=64)
-#     last_name = models.CharField(max_length=64)
-#     birthday_year = models.PositiveIntegerField()
-
-
-# class CustomUser(AbstractUser):
-
 
 class CustomUser(models.Model):  # AbstractBaseUser, PermissionsMixin
     # username = ASCIIUsernameValidator()
@@ -28,90 +17,3 @@ class CustomUser(models.Model):  # AbstractBaseUser, PermissionsMixin
     last_name = models.CharField(verbose_name="Фамилия", max_length=150)
     birthday_year = models.PositiveIntegerField(verbose_name="День рождения", blank=True, null=True)
     email = models.EmailField(verbose_name="Почта", unique=True)
-
-    # USERNAME_FIELD = 'email'
-    # REQUIRED_FIELDS = []
-    #
-    # objects = UserManager()
-    #
-    # def __str__(self):
-    #     return self.email
-
-
-    
-    # email = models.EmailField(("email address"), max_length=256, unique=True,
-    #                          error_messages={
-    #                              "unique": ("A user with that email address already exists."),
-    #                          }, )
-
-    # class Meta:
-    #     ordering = ['created']
-
-# class CustomUser(AbstractBaseUser, PermissionsMixin):
-#     username_validator = ASCIIUsernameValidator()
-#
-#     username = models.CharField(
-#         _("username"),
-#         max_length=150,
-#         unique=True,
-#         help_text=_("Required. 150 characters or fewer. ASCII letters and digits only."),
-#         validators=[username_validator],
-#         error_messages={
-#             "unique": _("A user with that username already exists."),
-#         },
-#     )
-#     first_name = models.CharField(_("first name"), max_length=150, blank=True)
-#     last_name = models.CharField(_("last name"), max_length=150, blank=True)
-#     age = models.PositiveIntegerField(blank=True, null=True)
-#     #avatar = models.ImageField(upload_to=users_avatars_path, blank=True, null=True)
-#     email = models.CharField(
-#         _("email address"),
-#         max_length=256,
-#         unique=True,
-#         error_messages={
-#             "unique": _("A user with that email address already exists."),
-#         },
-#     )
-#     is_staff = models.BooleanField(
-#         _("staff status"),
-#         default=False,
-#         help_text=_("Designates whether the user can log into this admin site."),
-#     )
-#     is_active = models.BooleanField(
-#         _("active"),
-#         default=True,
-#         help_text=_(
-#             "Designates whether this user should be treated as active. \
-#             Unselect this instead of deleting accounts."
-#         ),
-#     )
-#     date_joined = models.DateTimeField(_("date joined"), auto_now_add=True)
-#
-#     objects = UserManager()
-#
-#     EMAIL_FIELD = "email"
-#     USERNAME_FIELD = "username"
-#     REQUIRED_FIELDS = ["email"]
-#
-#     class Meta:
-#         verbose_name = _("user")
-#         verbose_name_plural = _("users")
-#
-#     def clean(self):
-#         super().clean()
-#         self.email = self.__class__.objects.normalize_email(self.email)
-#
-#     def get_full_name(self):
-#         """
-#         Return the first_name plus the last_name, with a space in between.
-#         """
-#         full_name = "%s %s" % (self.first_name, self.last_name)
-#         return full_name.strip()
-#
-#     def get_short_name(self):
-#         """Return the short name for the user."""
-#         return self.first_name
-#
-#     def email_user(self, subject, message, from_email=None, **kwargs):
-#         """Send an email to this user."""
-#         send_mail(subject, message, from_email, [self.email], **kwargs)
